23f8de7a5293572007d7eae5ea60160c41abc0ef
John Guest <jcguest90@gmail.com>
2019-09-13 17:46:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,8 +10,12 @@ end
 
 def match(matches)
   matches.each do |possible_match|
+    if
   possible_match.split("").sort == @word.split("").sort
-  
+    return possible_match
+  else
+    return nil
+  end
   binding.pry
 end
 end
